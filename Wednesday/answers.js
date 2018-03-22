38904e987e147e9b94346521125534ad74ccf219
bartwiharto <bartwiharto@gmail.com>
2018-03-21 18:40:51 -0600

    Commit 3 - Excited Kittens answered

--- Wednesday/answers.js
+++ Wednesday/answers.js
@@ -6,12 +6,34 @@
 
 //Get Even:
 
-for(let i = 0; i <= 200; i += 2) {
+// for(let i = 0; i <= 200; i += 2) {
+// 	console.log(i);
+// }
+
+//Damn Kitten:
+
+// 1.
+// 
+//for(let i = 1; i <= 20; i++) {
+// 	console.log("Love me, pet me! HSSSSSS!");
+// }
+
+// 2. 
+
+// var i = 0; 
+// const catTalk = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+
+// Math.floor(Math.random()*3);
+
+
+// for(let i = 0; i <= 20; i++) {
+// 	console.log(i);
+
+// 3.
+
+for(let i = 1; i <= 100; i++) {
 	console.log(i);
 }
-
-
-
 
 
 
