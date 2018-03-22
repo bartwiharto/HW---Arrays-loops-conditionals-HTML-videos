f65bf70f5c45a81d30969f4c48d573e56625e7cf
bartwiharto <bartwiharto@gmail.com>
2018-03-21 19:40:17 -0600

    Commit 5 - Getting to Know You answered

--- Wednesday/answers.js
+++ Wednesday/answers.js
@@ -31,17 +31,51 @@
 
 // 3. FizzBuzz
 
-for(let i = 1; i <= 100; i++) {
-	if (i % 3 === 0 && i % 5 === 0){
-		console.log("FizzBuzz");
-	} else if (i % 3 === 0) { 
-		console.log("Fizz");
-	} else if (i % 5 === 0) {
-		console.log("Buzz");
-	} else {
-		console.log(i);
-	}
-}
+// for(let i = 1; i <= 100; i++) {
+// 	if (i % 3 === 0 && i % 5 === 0){
+// 		console.log("FizzBuzz");
+// 	} else if (i % 3 === 0) { 
+// 		console.log("Fizz");
+// 	} else if (i % 5 === 0) {
+// 		console.log("Buzz");
+// 	} else {
+// 		console.log(i);
+// 	}
+// }
+
+// Getting to know you:
+
+const thom = ["Thom", 1000, "Christchurch"];
+const karolin = ["Karolin", 16, "New York"];
+const kristyn = ["Kristyn", 5, "Pittsburgh"];
+const matt = ["Matt H", 186, "Philadelphia"];
+
+//1.
+thom [0] = "Gameboy";
+//2.
+karolin [1] = 17;
+//3.
+matt [2] = "Gotham City";
+//4.
+krystin [2] = "Brooklyn";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
